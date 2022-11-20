3d08776995933eb569d21bf2c5c6f4a6374a2afb
Riiyabatra <batra.riy@northeastern.edu>
2022-11-20 15:35:39 -0500

    removing warnings

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -19,7 +19,7 @@ export const createTuit = async (tuit) => {
 }
 
 export const updateTuit = async (tuit) => {
-    const response = await axios
+    await axios
         .put(`${TUITS_API}/${tuit._id}`, tuit);
     return tuit;
 }
